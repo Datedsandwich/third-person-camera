2b589709df824d3b3cca4d85d3709d0b7222931c
Luke Robinson <robinsonlm@live.co.uk>
2017-04-17 10:47:45 +0100

    Removed comments that provide no value, renamed variables to be consistent in styling, tooltipped everything and made variables private where possible

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,233 +1,171 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraMovement : MonoBehaviour 
-{
-	// ----------------------------------------------- Data members ----------------------------------------------
-	[Tooltip("What the camera will look at.")]
-	public Transform target;			// What the camera will be looking at
-	[Tooltip("How far the camera currently is from the target.")]
-	public float distance = 10.0f;		// How far the camera is from the target
-	[Tooltip("How fast the camera moves horizontally.")]
-	public float xSpeed = 10.0f;		// Longtitudal speed of camera
-	[Tooltip("How fast the camera moves vertically.")]
-	public float ySpeed = 10.0f;		// Latitudal speed of camera
+public class CameraMovement : MonoBehaviour {
+	[Tooltip ("What the camera will look at."), SerializeField]
+	private Transform target;
+	[Tooltip ("How far the camera currently is from the target."), SerializeField]
+	private float distance = 10.0f;
+	[Tooltip ("How fast the camera moves horizontally."), SerializeField]
+	private float xSpeed = 10.0f;
+	[Tooltip ("How fast the camera moves vertically."), SerializeField]
+	private float ySpeed = 10.0f;
 
-	// yMinLimit and yMaxLimit are used to clamp the y angle of the camera.
-	[Tooltip("Minimum angle of the camera on the y axis.")]
-	public float yMinLimit = 10f;
-	[Tooltip("Maximum angle of the camera on the y axis.")]
-	public float yMaxLimit = 80f;
+	[Tooltip ("Minimum angle of the camera on the y axis."), SerializeField]
+	private float yMinLimit = 10f;
+	[Tooltip ("Maximum angle of the camera on the y axis."), SerializeField]
+	private float yMaxLimit = 80f;
 
-	[Tooltip("Minimum angle of the camera on the x axis.")]
-	public float xMinLimit = -360f;
-	[Tooltip("Maximum angle of the camera on the x axis.")]
-	public float xMaxLimit = 360f;
+	[Tooltip ("Minimum angle of the camera on the x axis."), SerializeField]
+	private float xMinLimit = -360f;
+	[Tooltip ("Maximum angle of the camera on the x axis."), SerializeField]
+	private float xMaxLimit = 360f;
 
-	[Tooltip("Minimum allowed distance between camera and target.")]
-	public float distanceMin = 0.5f;			// Minimum distance camera can be from target
-	[Tooltip("Maximum allowed distance between camera and target")]
-	public float distanceMax = 10f;				// Maximum distance camera can be from target
+	[Tooltip ("Minimum allowed distance between camera and target."), SerializeField]
+	private float distanceMin = 0.5f;
+	[Tooltip ("Maximum allowed distance between camera and target"), SerializeField]
+	private float distanceMax = 10f;
 
-	[Tooltip("Radius of the thin SphereCast, used to detect camera collisions.")]
-	public float thinRadius = 0.15f;
-	[Tooltip("Radius of the thick SphereCast, used to detect camera collisions.")]
-	public float thickRadius = 0.3f;
-	[Tooltip("LayerMask used for detecting camera collisions. Camera will not avoid objects if this is not set correctly.")]
-	public LayerMask layerMask;
+	[Tooltip ("Radius of the thin SphereCast, used to detect camera collisions."), SerializeField]
+	private float thinRadius = 0.15f;
+	[Tooltip ("Radius of the thick SphereCast, used to detect camera collisions."), SerializeField]
+	private float thickRadius = 0.3f;
+	[Tooltip ("LayerMask used for detecting camera collisions. Camera will not avoid objects if this is not set correctly."), SerializeField]
+	private LayerMask layerMask;
 
-	private Quaternion rotation;		// Local reference to rotation
-	private Vector3 position;			// Local reference to position
-	float x = 0.0f;						// x angle of camera
-	float y = 0.0f;						// y angle of camera
-	// ----------------------------------------------- End Data members -------------------------------------------
+	private Quaternion rotation;
+	private Vector3 position;
+	private float x = 0.0f;
+	private float y = 0.0f;
 
-	// --------------------------------------------------- Methods ------------------------------------------------
-	// --------------------------------------------------------------------
-	// Use this for initialization
-	void Start () 
-	{
-		// Going to use a default Unity functionality for this.
+	void Start () {
 		Vector3 angles = this.transform.eulerAngles;
 		x = angles.y;
 		y = angles.x;
 	}
-	// --------------------------------------------------------------------
-	void LateUpdate () 
-	{
-		if (target)		// Does target exist? (Not Null)
-		{
-			CameraMove();							// Move the camera, using the mouse.
-			rotation = Quaternion.Euler(y, x, 0);	// Returns a rotation that rotates z degrees around the z axis, x degrees around the x axis, and y degrees around the y axis (in that order).
 
-			// Zoom out if we're not at maximum zoom distance.
-			if(distance < distanceMax)
-			{
-				distance = Mathf.Lerp(distance, distanceMax, Time.deltaTime * 2f);
+	void LateUpdate () {
+		if (target) {
+			CameraMove ();
+			rotation = Quaternion.Euler (y, x, 0);
+
+			if (distance < distanceMax) {
+				distance = Mathf.Lerp (distance, distanceMax, Time.deltaTime * 2f);
 			}
-			// We'll declare a new Vector3 storing -distance. This will be 0 if we can see Bip, so nothing will happen.
-			// However, if we can't see Bip, position.z will be equal to distance * -1, flipping it.
-			Vector3 distanceVector = new Vector3(0.0f, 0.0f, -distance);
-			// Camera follows target
+
+			Vector3 distanceVector = new Vector3 (0.0f, 0.0f, -distance);
 			Vector3 position = rotation * distanceVector + target.position;
 			transform.rotation = rotation;
 			transform.position = position;
-			CameraCollision();
+			CameraCollision ();
 		}
 	}
-	// --------------------------------------------------------------------
-	public void CameraMove()
-	{
-		// Offset the angles by the mouse, when the mouse is moved.
-		x += Input.GetAxis("Mouse X") * xSpeed;
-		y -= Input.GetAxis("Mouse Y") * ySpeed;
 
-		x = ClampAngle(x, xMinLimit, xMaxLimit);
-		y = ClampAngle(y, yMinLimit, yMaxLimit);
+	public void CameraMove () {
+		x += Input.GetAxis ("Mouse X") * xSpeed;
+		y -= Input.GetAxis ("Mouse Y") * ySpeed;
+
+		x = ClampAngle (x, xMinLimit, xMaxLimit);
+		y = ClampAngle (y, yMinLimit, yMaxLimit);
 	}
-	// --------------------------------------------------------------------
-	public float ClampAngle(float angle, float min, float max)
-	{
-		// Ensure that angle is between -360 and 360, because it is a float and can be any size.
-		// If you rotate the camera 360 degrees in either direction, angle will snap back to 0.
-		if (angle < -360F)
-		{
+
+	public float ClampAngle (float angle, float min, float max) {
+		if (angle < -360F) {
 			angle += 360F;
 		}
 
-		if (angle > 360F)
-		{
+		if (angle > 360F) {
 			angle -= 360F;
 		}
-		// Then call Mathf.Clamp to actually clamp the angle.
-		return Mathf.Clamp(angle, min, max);
+
+		return Mathf.Clamp (angle, min, max);
 	}
-	// -------------------------------------------------------------------
-	void CameraCollision ()
-	{
-		Vector3 normal, thickNormal;						// Normal of the cast collisions.
-		Vector3 ray = transform.position - target.position;	// Direction for the SphereCasts.
 
-		// The position of the thin SphereCast collision.
-		Vector3 CollisionPoint = GetDoubleSphereCastCollision (transform.position, thinRadius, out normal, true);
-		// The position of the thick SphereCast collision.
-		Vector3 CollisionPointThick = GetDoubleSphereCastCollision (transform.position, thickRadius, out thickNormal, false);
-		// The position of the RayCast collision.
-		Vector3 CollisionPointRay = GetRayCollisionPoint (transform.position);
+	void CameraCollision () {
+		Vector3 normal, thickNormal;
+		Vector3 ray = transform.position - target.position;
 
-		// Collision Position from thick SphereCast, projected onto the RayCast.
-		Vector3 CollisionPointProjectedOnRay = Vector3.Project (CollisionPointThick - target.position, ray.normalized) + target.position;
-		// Direction to push the camera.
-		Vector3 VectorToProject = (CollisionPointProjectedOnRay - CollisionPointThick).normalized;
-		// Thick Collision Position projected onto thin SphereCast.
-		Vector3 CollisionPointThickProjectedOnThin = CollisionPointProjectedOnRay - VectorToProject * thinRadius;
-		// Distance between thick sphere and thin sphere collisions. Used to calculate where to push Camera.
-		float ThinToThickDistance = Vector3.Distance (CollisionPointThickProjectedOnThin, CollisionPointThick);
-		float ThinToThickDistanceNormal = ThinToThickDistance / (thickRadius - thinRadius);
+		Vector3 collisionPoint = GetDoubleSphereCastCollision (transform.position, thinRadius, out normal, true);
+		Vector3 collisionPointThick = GetDoubleSphereCastCollision (transform.position, thickRadius, out thickNormal, false);
+		Vector3 collisionPointRay = GetRayCollisionPoint (transform.position);
 
-		// Distance between target and thin sphere collision.
-		float CollisionDistanceThin = Vector3.Distance (target.position, CollisionPoint);
-		// Distance between target and Thick sphere collision.
-		float CollisionDistanceThick = Vector3.Distance (target.position, CollisionPointProjectedOnRay);
-		// Smoothly interpolating between distance and new distance.
-		float CollisionDistance = Mathf.Lerp (CollisionDistanceThick, CollisionDistanceThin, ThinToThickDistanceNormal);
+		Vector3 collisionPointProjectedOnRay = Vector3.Project (collisionPointThick - target.position, ray.normalized) + target.position;
+		Vector3 vectorToProject = (collisionPointProjectedOnRay - collisionPointThick).normalized;
+		Vector3 collisionPointThickProjectedOnThin = collisionPointProjectedOnRay - vectorToProject * thinRadius;
+		float thinToThickDistance = Vector3.Distance (collisionPointThickProjectedOnThin, collisionPointThick);
+		float thinToThickDistanceNormal = thinToThickDistance / (thickRadius - thinRadius);
+
+		float collisionDistanceThin = Vector3.Distance (target.position, collisionPoint);
+		float collisionDistanceThick = Vector3.Distance (target.position, collisionPointProjectedOnRay);
+
+		float collisionDistance = Mathf.Lerp (collisionDistanceThick, collisionDistanceThin, thinToThickDistanceNormal);
 
 		// Thick point can be actually projected IN FRONT of the character due to double projection to avoid sphere moving through the walls
 		// In this case we should only use thin point
-		bool isThickPointIncorrect = transform.InverseTransformDirection (CollisionPointThick - target.position).z > 0;
-		isThickPointIncorrect = isThickPointIncorrect || (CollisionDistanceThin < CollisionDistanceThick);
-		if (isThickPointIncorrect) 
-		{
-			CollisionDistance = CollisionDistanceThin;
+		bool isThickPointIncorrect = transform.InverseTransformDirection (collisionPointThick - target.position).z > 0;
+		isThickPointIncorrect = isThickPointIncorrect || (collisionDistanceThin < collisionDistanceThick);
+		if (isThickPointIncorrect) {
+			collisionDistance = collisionDistanceThin;
 		}
 
-		// if CollisionDistance is smaller than distance, zoom in
-		if (CollisionDistance < distance) 
-		{
-			distance = CollisionDistance;
+		if (collisionDistance < distance) {
+			distance = collisionDistance;
+		} else {
+			distance = Mathf.SmoothStep (distance, collisionDistance, Time.deltaTime * 100 * Mathf.Max (distance * 0.1f, 0.1f));
 		}
-		else
-		{
-			// Otherwise, zoom out.
-			distance = Mathf.SmoothStep (distance, CollisionDistance, Time.deltaTime * 100 * Mathf.Max (distance * 0.1f, 0.1f));
-		}
-		// Clamp distance to our min and max values.
-		distance = Mathf.Clamp(distance, distanceMin, distanceMax);
-		// Move the camera to avoid going through objects!!!!
+
+		distance = Mathf.Clamp (distance, distanceMin, distanceMax);
 		transform.position = target.position + ray.normalized * distance;
 
-		if (Vector3.Distance(target.position, CollisionPoint) > Vector3.Distance(target.position, CollisionPointRay))
-		{
-			transform.position = CollisionPointRay;
+		if (Vector3.Distance (target.position, collisionPoint) > Vector3.Distance (target.position, collisionPointRay)) {
+			transform.position = collisionPointRay;
 		} 
 	}
-	// -------------------------------------------------------------------
-	Vector3 GetDoubleSphereCastCollision(Vector3 cameraPosition, float radius, out Vector3 normal, bool pushAlongNormal)
-	{
-		// Double Sphere Casting.
-		// Length of the cast.
+
+	Vector3 GetDoubleSphereCastCollision (Vector3 cameraPosition, float radius, out Vector3 normal, bool pushAlongNormal) {
 		float rayLength = 1;
 
 		RaycastHit hit;
-		// Local reference to target.position
 		Vector3 origin = target.position;
-		// Cast direction.
 		Vector3 ray = origin - cameraPosition;
-		// Dot product of transform.forward, and the ray.
-		float dot = Vector3.Dot(transform.forward, ray);
-		if (dot < 0)
-		{
+		float dot = Vector3.Dot (transform.forward, ray);
+
+		if (dot < 0) {
 			ray *= -1;
 		}
 
 		// Project the sphere in an opposite direction of the desired character->camera vector to get some space for the real spherecast
-		if (Physics.SphereCast(origin, radius, ray.normalized, out hit, rayLength, layerMask))
-		{
+		if (Physics.SphereCast (origin, radius, ray.normalized, out hit, rayLength, layerMask)) {
 			origin = origin + ray.normalized * hit.distance;
-		}
-		else
-		{
+		} else {
 			origin += ray.normalized * rayLength;
 		}
 
 		// Do final spherecast with offset origin
 		ray = origin - cameraPosition;
-		if (Physics.SphereCast(origin, radius, -ray.normalized, out hit, ray.magnitude, layerMask))
-		{
+		if (Physics.SphereCast (origin, radius, -ray.normalized, out hit, ray.magnitude, layerMask)) {
 			normal = hit.normal;
 
-			if(pushAlongNormal)
-			{
-				return hit.point + hit.normal*radius;
-			}
-			else
-			{
+			if (pushAlongNormal) {
+				return hit.point + hit.normal * radius;
+			} else {
 				return hit.point;
 			}
-		}
-		else
-		{
+		} else {
 			normal = Vector3.zero;
 			return cameraPosition;
 		}
 	}
-	// -------------------------------------------------------------------
-	Vector3 GetRayCollisionPoint(Vector3 cameraPosition)
-	{
-		// Local reference to target.position
+
+	Vector3 GetRayCollisionPoint (Vector3 cameraPosition) {
 		Vector3 origin = target.position;
-		// Direction for raycast
 		Vector3 ray = cameraPosition - origin;
 
 		RaycastHit hit;
-		if (Physics.Raycast(origin, ray.normalized, out hit, ray.magnitude, layerMask))
-		{
-			// Return position of hit + the normal of that hit, multiplied by a smoothing variable.
+		if (Physics.Raycast (origin, ray.normalized, out hit, ray.magnitude, layerMask)) {
 			return hit.point + hit.normal * 0.15f;
 		}
-		// or we just return the camera position we passed in.
+
 		return cameraPosition;
 	}
-	// -------------------------------------------------------------------
-	// --------------------------------------------------- End Methods ---------------------------------------------
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,139 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement: MonoBehaviour
-{
-	// ----------------------------------------------- Data members ----------------------------------------------
-	public float speed = 10;
-	private Vector3 movement;
-	public float jumpVelocity = 20;			// For jump height.
-	public float jumpReduction = 10;		// The degree to which variable jump is variable.
-	public Vector3 maxVelocityCap;			// To cap velocity.
-
-	private bool hasJumped = false;				// To check if player has pressed jump.
-	private bool cutJumpShort = false;			// If true, player has stopped holding button.
+public class PlayerMovement: MonoBehaviour {
+	[Tooltip("Is this character currently controllable by the player"), SerializeField]
 	public bool isHandlingInput = true;
 
-	public Rigidbody rigidbody;
-	public LayerMask layerMask;
+	[Tooltip("The speed the player will move"), SerializeField]
+	private float speed = 10;
+	private Vector3 movement;
+	[Tooltip("The amount of upwards force to apply to the character when they jump"), SerializeField]
+	private float jumpVelocity = 20;
+	[Tooltip("If the player lets go of the jump button, their y velocity will be reduced to this number."), SerializeField]
+	private float jumpReduction = 10;
+	[Tooltip("The maximum velocity the character should be able to reach"), SerializeField]
+	private Vector3 maxVelocity;
 
+	private bool hasJumped = false;
+	private bool cutJumpShort = false;
 	private bool isGrounded;
-	// ----------------------------------------------- End Data members ------------------------------------------
 
-	// --------------------------------------------------- Methods -----------------------------------------------
-	// --------------------------------------------------------------------
-	void Awake()
-	{
-		// Set up references.
-		rigidbody = GetComponent<Rigidbody>();
+	private Rigidbody rigidbody;
+	[Tooltip("The character will consider anything in this LayerMask to be 'Ground'"), SerializeField]
+	private LayerMask groundLayerMask;
+
+	void Awake () {
+		rigidbody = GetComponent<Rigidbody> ();
 	}
-	// --------------------------------------------------------------------
-	// Put physics stuff in here.
-	void FixedUpdate()
-	{
-		ApplyJumpPhysics();
-		CapVelocity();
+
+	void FixedUpdate () {
+		ApplyJumpPhysics ();
+		CapVelocity ();
 	}
-	// --------------------------------------------------------------------
-	// Called once every frame.
-	void Update()
-	{
-		if (movement != Vector3.zero) 
-		{
-			// If the object is moving, rotate it to face the direction it is moving.
+		
+	void Update () {
+		if (movement != Vector3.zero) {
 			rigidbody.transform.rotation = Quaternion.LookRotation (movement);
 		}
 
-		// Create a sphere at the player's feet. If the sphere collides with anything on the layer(s) layerMask
-		// return true
-		if (Physics.CheckSphere(transform.position, 1f, layerMask))
-		{
-			// If sphere collides, we're touching ground
+		if (Physics.CheckSphere (transform.position, 1f, groundLayerMask)) {
 			isGrounded = true;
-		}
-		else
-		{
-			// otherwise, we're in the air
+		} else {
 			isGrounded = false;
 		}
-		// Input handling.
-		float horizontalInput = Input.GetAxis("Horizontal");
-		float verticalInput = Input.GetAxis("Vertical");
+
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
 		ManageMovement (horizontalInput, verticalInput);
 
-		if (Input.GetButtonDown ("Jump") && isGrounded) 
-		{
-			// If the player is grounded and they press Jump, make them jump!
-			Jump();
+		if (Input.GetButtonDown ("Jump") && isGrounded) {
+			Jump ();
 		}
 
-		// Cut jump short for variable height.
-		if (Input.GetButtonUp ("Jump") && !isGrounded) 
-		{
-			CutJumpShort();
+		if (Input.GetButtonUp ("Jump") && !isGrounded) {
+			CutJumpShort ();
 		}
 	}
-	// --------------------------------------------------------------------
-	// To handle movement, incorporate pulling of objects later.
-	public void ManageMovement(float h, float v)
-	{
-		if(!isHandlingInput)
-		{
-			// If we don't want to handle Input for any reason, return.
+
+	public void ManageMovement (float h, float v) {
+		if (!isHandlingInput) {
 			return;
 		}
-		// Find the new forward and right vectors to move along.
-		Vector3 forwardMove = Vector3.Cross(Camera.main.transform.right, Vector3.up);
+
+		Vector3 forwardMove = Vector3.Cross (Camera.main.transform.right, Vector3.up);
 		Vector3 horizontalMove = Camera.main.transform.right;
 
-		// Multiply the direction vectors by the Input.GetAxis floats.
 		movement = forwardMove * v + horizontalMove * h;
 
-		// Normalise the movement vector and make it proportional to the speed per second.
 		movement = movement.normalized * speed * Time.deltaTime;
-		// Move the player to it's current position plus this movement.
-		rigidbody.MovePosition(transform.position + movement);
+		rigidbody.MovePosition (transform.position + movement);
 	}
-	// --------------------------------------------------------------------
-	// To make jump.
-	public void Jump() 
-	{
+
+	public void Jump () {
 		hasJumped = true;
 	}
-	// --------------------------------------------------------------------
-	// To make jump variable.
-	public void CutJumpShort() 
-	{
+
+	public void CutJumpShort () {
 		cutJumpShort = true;
 	}
-	// --------------------------------------------------------------------
-	private void ApplyJumpPhysics()
-	{
-		if (hasJumped)
-		{
-			rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpVelocity, rigidbody.velocity.z);
+
+	private void ApplyJumpPhysics () {
+		if (hasJumped) {
+			rigidbody.velocity = new Vector3 (rigidbody.velocity.x, jumpVelocity, rigidbody.velocity.z);
 			hasJumped = false;
 		}
 
-		// Cancel the jump when the button is no longer pressed
-		if (cutJumpShort)
-		{
-			if (rigidbody.velocity.y > jumpReduction)
-			{
-				rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpReduction, rigidbody.velocity.z);
+		if (cutJumpShort) {
+			if (rigidbody.velocity.y > jumpReduction) {
+				rigidbody.velocity = new Vector3 (rigidbody.velocity.x, jumpReduction, rigidbody.velocity.z);
 			}
 			cutJumpShort = false;
 		}
 	}
-	// --------------------------------------------------------------------
-	// To cap velocity so  doesn't fall too fast.
-	void CapVelocity() 
-	{
-		Vector3 _velocity = GetComponent<Rigidbody>().velocity;
-		_velocity.x = Mathf.Clamp (_velocity.x, -maxVelocityCap.x, maxVelocityCap.x);
-		_velocity.y = Mathf.Clamp (_velocity.y, -maxVelocityCap.y, maxVelocityCap.y);
-		_velocity.z = Mathf.Clamp (_velocity.z, -maxVelocityCap.z, maxVelocityCap.z);
+
+	void CapVelocity () {
+		Vector3 _velocity = GetComponent<Rigidbody> ().velocity;
+		_velocity.x = Mathf.Clamp (_velocity.x, -maxVelocity.x, maxVelocity.x);
+		_velocity.y = Mathf.Clamp (_velocity.y, -maxVelocity.y, maxVelocity.y);
+		_velocity.z = Mathf.Clamp (_velocity.z, -maxVelocity.z, maxVelocity.z);
 		rigidbody.velocity = _velocity;
 	}
-	// --------------------------------------------------- End Methods --------------------------------------------
 }
